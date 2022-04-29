ab24d36f4dce779af2f8b30467dabc7f075d6973
codefug <86544979+codefug@users.noreply.github.com>
2022-04-29 12:17:01 +0900

    see you later
    g

--- python/백준 풀이/실4 수 찾기.py
+++ python/백준 풀이/실4 수 찾기.py
@@ -1,1 +1,32 @@
 #1920 수 찾기
+
+n=int(input())
+
+n_l=sorted(list(map(int,input().split())))
+
+m=int(input())
+
+m_l=list(map(int,input().split()))
+
+def two(l,start,end):
+    
+    if start>end:
+        
+        return 0
+    
+    mid=(start+end)//2
+    
+    if n_l[mid]==l:
+        
+        return 1
+    
+    if n_l[mid]>l:
+        
+        return two(l,start,mid-1)
+        
+    else:
+        
+        return two(l,mid+1,end)
+    
+for i in m_l:
+    print(two(i,0,len(n_l)-1))
