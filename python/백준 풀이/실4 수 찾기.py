a26353e7ce8d5380eafe7b1c265158549d86b800
codefug <86544979+codefug@users.noreply.github.com>
2022-04-29 20:09:52 +0900

    Update 실4 수 찾기.py

--- python/백준 풀이/실4 수 찾기.py
+++ python/백준 풀이/실4 수 찾기.py
@@ -2,12 +2,32 @@
 
 n=int(input())
 
+<<<<<<< Updated upstream
 n_l=sorted(list(map(int,input().split())))
+=======
+<<<<<<< HEAD
+n_l=list(map(int,input().split()))
+=======
+n_l=sorted(list(map(int,input().split())))
+>>>>>>> ab24d36f4dce779af2f8b30467dabc7f075d6973
+>>>>>>> Stashed changes
 
 m=int(input())
 
 m_l=list(map(int,input().split()))
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+n_l.sort()
+
+m_l.sort()
+
+start=0
+end=len(m_l)
+
+=======
+>>>>>>> Stashed changes
 def two(l,start,end):
     
     if start>end:
@@ -29,4 +49,9 @@ def two(l,start,end):
         return two(l,mid+1,end)
     
 for i in m_l:
+<<<<<<< Updated upstream
     print(two(i,0,len(n_l)-1))
+=======
+    print(two(i,0,len(n_l)-1))
+>>>>>>> ab24d36f4dce779af2f8b30467dabc7f075d6973
+>>>>>>> Stashed changes
