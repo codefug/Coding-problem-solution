b8edfb33688c3ce9d7caa860ff6bd4eb482dee6f
codefug <86544979+codefug@users.noreply.github.com>
2022-05-04 16:54:06 +0900

    Update 골1 12969 ABC.py

--- python/백준 풀이/골1 12969 ABC.py
+++ python/백준 풀이/골1 12969 ABC.py
@@ -2,3 +2,36 @@
 
 n,k=map(int,input().split())
 
+'''
+
+https://hjp845.tistory.com/165
+dp[a][b][쌍의 개수]=0 or 1가 맞고
+
+a b c 하나씩 넣는거 시작
+
+dp[a][b][]         
+
+for문으로 할수 있는가?
+
+안됨 왜냐 순서대로 되는게 아님 ㅇㅇ
+
+가지 뻗듯이 나가야함
+'''
+
+def solve(i,a,b,t):
+    
+    if t==n:
+        
+        if i==k:
+            
+            return 1
+        
+        else:
+            
+            return 0
+        
+    if dp[i][a][b][t]:
+        
+        return 0
+    
+    
